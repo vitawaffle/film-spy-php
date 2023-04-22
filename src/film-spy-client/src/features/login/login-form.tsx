698f60dfa1df0af4ebcf30ab9d97ff0c7a3c74a1
Vitali Labatsevich <vitaly.lobatsevich@gmail.com>
2023-04-23 01:10:41 +0300

    Create login-form.tsx and schema.ts

---
+++ src/film-spy-client/src/features/login/login-form.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import yup from 'schema';
+
+const LoginForm = () => {
+  return (
+    <form>
+    </form>
+  );
+};
+
+export default LoginForm;
---
+++ src/film-spy-client/src/schema.ts
@@ -0,0 +1,6 @@
+import * as yup from 'yup';
+
+declare module 'yup' {
+}
+
+export default yup;
