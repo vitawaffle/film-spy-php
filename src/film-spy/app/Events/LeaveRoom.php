4423834f36bb4ae70163ecc47c0f097ddbd3bc07
Vitaly Lobatsevich <vitaly.lobatsevich@easybrain.com>
2023-05-08 17:03:33 +0200

    Remove listeners

--- src/film-spy/app/Events/LeaveRoom.php
+++ src/film-spy/app/Events/LeaveRoom.php
@@ -11,7 +11,7 @@ use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class JoinRoom implements ShouldBroadcast
+class LeaveRoom implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
--- src/film-spy/app/Providers/EventServiceProvider.php
+++ src/film-spy/app/Providers/EventServiceProvider.php
@@ -2,8 +2,6 @@
 
 namespace App\Providers;
 
-use App\Events\JoinRoom;
-use App\Listeners\{BroadcastJoinRoom, BroadcastOutRoom};
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -19,10 +17,6 @@ class EventServiceProvider extends ServiceProvider
     protected $listen = [
         Registered::class => [
             SendEmailVerificationNotification::class,
-        ],
-        JoinRoom::class => [
-            BroadcastJoinRoom::class,
-            BroadcastOutRoom::class,
         ],
     ];
 
