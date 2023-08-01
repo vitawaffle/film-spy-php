6fa072cd5610f7872922ce49a44b213cb74e73d8
Vitaly Lobatsevich <vitaly.lobatsevich@easybrain.com>
2023-08-01 18:05:39 +0200

    Hide Rooms card from unauthorized user

--- src/film-spy-client/src/pages/home.tsx
+++ src/film-spy-client/src/pages/home.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Card, CardContent, Stack, Typography, Button } from '@mui/material';
 import { Create as CreateIcon, Refresh as RefreshIcon } from '@mui/icons-material';
 
-import { selectIsRoomsLoading } from 'app-slice';
+import { selectIsAuthenticated, selectIsRoomsLoading } from 'app-slice';
 import { useAppSelector } from 'hooks';
 import {
   RoomList,
@@ -13,6 +13,7 @@ import {
 import { strings } from 'localization';
 
 const Home = (): JSX.Element => {
+  const isAuthenticated = useAppSelector(selectIsAuthenticated);
   const isRoomsLoading = useAppSelector(selectIsRoomsLoading);
   const [isCreateRoomModalOpen, setIsCreateRoomModalOpen] = useState(false);
   const loadRooms = useLoadRooms();
@@ -28,39 +29,43 @@ const Home = (): JSX.Element => {
   };
 
   return (
-    <Card>
-      <CardContent>
-        <Stack spacing={3}>
-          <CreateRoomModal
-            isOpen={isCreateRoomModalOpen}
-            onSuccess={closeCreateRoomModal}
-            onClose={closeCreateRoomModal}
-          />
-          <JoinRoomModal />
-          <Typography variant="h3" component="h3">
-            {strings.common.rooms}
-          </Typography>
-          <Stack spacing={2} direction="row">
-            <Button
-              onClick={handleCreateRoomClick}
-              variant="contained"
-              startIcon={<CreateIcon />}
-            >
-              {strings.common.create}
-            </Button>
-            <Button
-              onClick={handleRefreshClick}
-              variant="outlined"
-              startIcon={<RefreshIcon />}
-              disabled={isRoomsLoading}
-            >
-              {strings.common.refresh}
-            </Button>
-          </Stack>
-          <RoomList />
-        </Stack>
-      </CardContent>
-    </Card>
+    <>
+      {isAuthenticated && (
+        <Card>
+          <CardContent>
+            <Stack spacing={3}>
+              <CreateRoomModal
+                isOpen={isCreateRoomModalOpen}
+                onSuccess={closeCreateRoomModal}
+                onClose={closeCreateRoomModal}
+              />
+              <JoinRoomModal />
+              <Typography variant="h3" component="h3">
+                {strings.common.rooms}
+              </Typography>
+              <Stack spacing={2} direction="row">
+                <Button
+                  onClick={handleCreateRoomClick}
+                  variant="contained"
+                  startIcon={<CreateIcon />}
+                >
+                  {strings.common.create}
+                </Button>
+                <Button
+                  onClick={handleRefreshClick}
+                  variant="outlined"
+                  startIcon={<RefreshIcon />}
+                  disabled={isRoomsLoading}
+                >
+                  {strings.common.refresh}
+                </Button>
+              </Stack>
+              <RoomList />
+            </Stack>
+          </CardContent>
+        </Card>
+      )}
+    </>
   );
 };
 
